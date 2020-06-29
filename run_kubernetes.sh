93887c29abb3ac9605d9df96c181eb2fec083061
surelay <hooddevelopers@gmail.com>
2020-06-29 00:58:13 +0000

    added all files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=surelay/udacity-project-five:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-project-five --port=80 --image=$dockerpath  
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-project-five-5c65fb9fd6-qwccx 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=surelay/udacity-project-five:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u surelay 
+docker tag udacity-project-five $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
